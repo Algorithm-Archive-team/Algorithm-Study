9e55cdb4745c2f0cad3f4d3b5a61aadb20977b27
tlstkdgns09 <116625144+tlstkdgns09@users.noreply.github.com>
2026-01-28 19:30:38 +0900

    Update and rename 문제번호_문제이름.java to 14916_거스름돈.java

---
+++ week01/14916_거스름돈.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+ public static void main(String[] args) {
+   Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int count = 0;
+   
+     while(n>0) {
+         if (n%5==0) {
+            count += n/5;
+            System.out.print(count);
+            return;
+         }
+      n -= 2;
+      count++;   
+     }
+     
+     if(n<0) {
+         System.out.print(-1);
+    } else {
+         System.out.print(count);
+     }
+}
+}
